201f4392ae27e7948cbb47f0596c89ad671e97c3
vishank91 <vishankchauhan@gmail.com>
2026-06-24 10:53:46 +0530

    24-05-2026

--- src/Pages/Authentication/SignupPage.jsx
+++ src/Pages/Authentication/SignupPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignupPage() {
     return (
         <>
-            <div class="authentication-wrapper authentication-cover authentication-bg">
-                <div class="authentication-inner row">
+            <div className="authentication-wrapper authentication-cover authentication-bg">
+                <div className="authentication-inner row">
                     {/*  Left Text  */}
-                    <div class="d-none d-lg-flex col-lg-7 p-0">
-                        <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+                    <div className="d-none d-lg-flex col-lg-7 p-0">
+                        <div className="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
                             <img
                                 src="/assets/img/illustrations/auth-register-illustration-light.png"
                                 alt="auth-register-cover"
-                                class="img-fluid my-5 auth-illustration"
+                                className="img-fluid my-5 auth-illustration"
                                 data-app-light-img="illustrations/auth-register-illustration-light.png"
                                 data-app-dark-img="illustrations/auth-register-illustration-dark.png"
                             />
@@ -19,7 +20,7 @@ export default function SignupPage() {
                             <img
                                 src="/assets/img/illustrations/bg-shape-image-light.png"
                                 alt="auth-register-cover"
-                                class="platform-bg"
+                                className="platform-bg"
                                 data-app-light-img="illustrations/bg-shape-image-light.png"
                                 data-app-dark-img="illustrations/bg-shape-image-dark.png"
                             />
@@ -28,112 +29,83 @@ export default function SignupPage() {
                     {/*  Left Text  */}
 
                     {/*  Register  */}
-                    <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
-                        <div class="w-px-400 mx-auto">
+                    <div className="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+                        <div className="w-px-400 mx-auto">
                             {/*  Logo  */}
-                            <div class="app-brand mb-4">
-                                <a href="index.html" class="app-brand-link gap-2">
-                                    <span class="app-brand-logo demo">
-                                        <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                                                fill="#7367F0"
-                                            />
-                                            <path
-                                                opacity="0.06"
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-                                                fill="#161616"
-                                            />
-                                            <path
-                                                opacity="0.06"
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-                                                fill="#161616"
-                                            />
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                                                fill="#7367F0"
-                                            />
-                                        </svg>
-                                    </span>
-                                </a>
+                            <div className="app-brand mb-4">
+                                <Link to="/" className="app-brand-link gap-2 text-white fs-1">
+                                    Venuefy
+                                </Link>
                             </div>
                             {/*  /Logo  */}
-                            <h3 class="mb-1 fw-bold">Adventure starts here 🚀</h3>
-                            <p class="mb-4">Make your app management easy and fun!</p>
+                            <h3 className="mb-1 fw-bold">Adventure starts here 🚀</h3>
+                            <p className="mb-4">Make your app management easy and fun!</p>
 
-                            <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
-                                <div class="mb-3">
-                                    <label for="username" class="form-label">Username</label>
+                            <form id="formAuthentication" className="mb-3" action="index.html" method="POST">
+                                <div className="mb-3">
+                                    <label htmlFor="username" className="form-label">Username</label>
                                     <input
                                         type="text"
-                                        class="form-control"
+                                        className="form-control"
                                         id="username"
                                         name="username"
                                         placeholder="Enter your username"
-                                        autofocus
+                                        autoFocus
                                     />
                                 </div>
-                                <div class="mb-3">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input type="text" className="form-control" id="email" name="email" placeholder="Enter your email" />
                                 </div>
-                                <div class="mb-3 form-password-toggle">
-                                    <label class="form-label" for="password">Password</label>
-                                    <div class="input-group input-group-merge">
+                                <div className="mb-3 form-password-toggle">
+                                    <label className="form-label" htmlFor="password">Password</label>
+                                    <div className="input-group input-group-merge">
                                         <input
                                             type="password"
                                             id="password"
-                                            class="form-control"
+                                            className="form-control"
                                             name="password"
                                             placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
                                             aria-describedby="password"
                                         />
-                                        <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
+                                        <span className="input-group-text cursor-pointer"><i className="ti ti-eye-off"></i></span>
                                     </div>
                                 </div>
 
-                                <div class="mb-3">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
-                                        <label class="form-check-label" for="terms-conditions">
+                                <div className="mb-3">
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                                        <label className="form-check-label" htmlFor="terms-conditions">
                                             I agree to
                                             <a href="javascript:void(0);">privacy policy & terms</a>
                                         </label>
                                     </div>
                                 </div>
-                                <button class="btn btn-primary d-grid w-100">Sign up</button>
+                                <button className="btn btn-primary d-grid w-100">Sign up</button>
                             </form>
 
-                            <p class="text-center">
+                            <p className="text-center">
                                 <span>Already have an account?</span>
                                 <a href="auth-login-cover.html">
                                     <span>Sign in instead</span>
                                 </a>
                             </p>
 
-                            <div class="divider my-4">
-                                <div class="divider-text">or</div>
+                            <div className="divider my-4">
+                                <div className="divider-text">or</div>
                             </div>
 
-                            <div class="d-flex justify-content-center">
-                                <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
-                                    <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
+                            <div className="d-flex justify-content-center">
+                                <a href="javascript:;" className="btn btn-icon btn-label-facebook me-3">
+                                    <i className="tf-icons fa-brands fa-facebook-f fs-5"></i>
                                 </a>
 
-                                <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
-                                    <i class="tf-icons fa-brands fa-google fs-5"></i>
+                                <a href="javascript:;" className="btn btn-icon btn-label-google-plus me-3">
+                                    <i className="tf-icons fa-brands fa-google fs-5"></i>
                                 </a>
 
-                                <a href="javascript:;" class="btn btn-icon btn-label-twitter">
-                                    <i class="tf-icons fa-brands fa-twitter fs-5"></i>
+                                <a href="javascript:;" className="btn btn-icon btn-label-twitter">
+                                    <i className="tf-icons fa-brands fa-twitter fs-5"></i>
                                 </a>
                             </div>
                         </div>
